1446021688b08580815ffa38e9a2676ae5b73c5f
marcus pianco <marcus.ufal@gmail.com>
2019-06-25 01:32:05 -0300

    Create resource list/change projects

--- index.js
+++ index.js
@@ -22,4 +22,22 @@ server.post('/projects', (req, res) => {
   return res.json(projects);
 });
 
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  projects.forEach(projectChange => {
+    if (projectChange.id === id) {
+      projectChange.title = title;
+      return res.json(projectChange);
+    } else {
+      return res.json('Projeto não encontrado');
+    }
+  });
+});
+
 server.listen('3000');
